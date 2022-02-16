abeb30276dc2b5a61ac01d62b09b812323c3c6a4
Tak2009 <lnuk2009jp@gmail.com>
2022-02-16 16:24:17 +0000

    put method added

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -7,8 +7,10 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Student;
@@ -47,4 +49,13 @@ import com.example.demo.service.StudentService;
 		studentService.deleteStudent(studentId);
 	}
 	
+	//https://www.youtube.com/watch?v=9SGDpanrc8U&t=4979s.
+	@PutMapping(path = "{studentId}")
+	public void updateStudent(
+			@PathVariable("studentId") Long studentId,
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) String email) {
+		studentService.updateStudent(studentId, name, email);
+	}
+		
 }
